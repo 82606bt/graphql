ec94ec847d72f040e1328799ed668c82658d61e0
long duan <92902400+82606bt@users.noreply.github.com>
2022-01-10 17:20:15 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  post '/graphql', to: 'graphql#execute'
-
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute'
-
-    root to: redirect('graphiql')
+  if Rails.env.development? || Rails.env.staging?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
